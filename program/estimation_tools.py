3ca967f2014222887a8ec37c55284b5af5cba423
Evan Landau <evanlandau@live.com>
2019-11-13 21:16:40 -0800

    Added tools to estimate mass using HORIZONS data

--- program/estimation_tools.py
+++ program/estimation_tools.py
@@ -2,11 +2,11 @@
 
 import math
 
-def estimate_radius(magnitude, albedo = 0.2): #Equation taken from http://www.physics.sfasu.edu/astro/asteroids/sizemagnitude.html
+def estimate_radius(magnitude, albedo = 0.1): #Equation taken from http://www.physics.sfasu.edu/astro/asteroids/sizemagnitude.html
     magic_number = 1329/2
     return (magic_number/math.sqrt(albedo))*(10**(-0.2*magnitude))
 
-def estimate_mass(magnitude, spectral_type, albedo = 0.2): #Some equations from https://en.wikipedia.org/wiki/Standard_asteroid_physical_characteristics
+def estimate_mass(magnitude, spectral_type, albedo = 0.1): #Some equations from https://en.wikipedia.org/wiki/Standard_asteroid_physical_characteristics
     radius = estimate_radius(magnitude, albedo)
     #Densities are estimated based on spectral type, defaulting to the moderate value. Values in g/cm^3
     if (spectral_type == "C"):
