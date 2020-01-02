c09b9c5b147dabeb8d1cd314d989feb5a9eae949
Evan Landau <evanlandau@live.com>
2020-01-02 13:25:49 -0800

    Finished HORIZONS downloader based on AstroQuery

---
+++ program/bodies.py
@@ -0,0 +1,13 @@
+#Class for astronomical bodies
+
+class Body: #Class representing all astronomical bodies.
+    position = [0, 0, 0] #Position x, y, z (in AU)
+    velocity = [0, 0, 0] #Velocity x, y, z (in AU/day)
+    mass = 0
+    IAU_number = -1
+    def __init__(self, number, mass, x, y, z, dx, dy, dz):
+        self.IAU_number = number
+        self.position = [x, y, z]
+        self.velocity = [dx, dy, dz]
+        self.mass = mass
+    
---
+++ program/downloader.py
@@ -0,0 +1,18 @@
+#Function that actually downloads a given body's data
+#Takes in the body's IAU number, and the start time in MJD (and mass in kilograms, if a major body), returns a body object
+
+import bodies
+import estimation_tools
+#Uses astroquery
+from astroquery.jplhorizons import Horizons
+
+def download_Body(number, time, mass = -1):
+    majorbody = (mass != -1) #Tests if a mass value is provided, and if it is, marks body as major
+    if majorbody: #Creates actual body in astroquery
+        working_body = Horizons(id=str(number), epochs=time, id_type='majorbody')
+    else:
+        working_body = Horizons(id=str(number), epochs=time) 
+    body_table = working_body.vectors() #Returns astropy table of position/velocity vectors
+    if (mass == -1): #Estimates mass using function in estimation_tools.py
+        mass = estimation_tools.estimate_mass(body_table['H'][0], 'x')
+    return bodies.Body(number, mass, body_table['x'][0], body_table['y'][0], body_table['z'][0], body_table['vx'][0], body_table['vy'][0], body_table['vz'][0])
